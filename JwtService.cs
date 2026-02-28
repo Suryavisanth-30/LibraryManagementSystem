f090b6b763fd1c351eb1de93da0d5da346433abd
Suryavisanth <151948015+Suryavisanth-30@users.noreply.github.com>
2026-02-28 15:50:57 +0530

    Add files via upload

---
+++ JwtService.cs
@@ -0,0 +1,38 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LibraryManagement.Services
+{
+    public class JwtService
+    {
+
+        public readonly IConfiguration configuration;
+        public JwtService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public string generateToken(int userId , string role)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier,userId.ToString()),
+                new Claim(ClaimTypes.Role,role)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer: configuration["Jwt:Issuer"],
+                 audience: configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(
+                    Convert.ToDouble(configuration["Jwt:DurationInMinutes"])
+                ),
+                signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
