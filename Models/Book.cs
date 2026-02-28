a77f20e14f3de88d71de8ab90b89140044add1f3
Suryavisanth <151948015+Suryavisanth-30@users.noreply.github.com>
2026-02-28 15:47:54 +0530

    Update Book.cs

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,13 +1,28 @@
-﻿namespace LibraryManagement.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryManagement.Models
 {
+    [Table("books")]
     public class Book
     {
+        [Key]
+        [Column("book_id")]
         public int BookId { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Category { get; set; }
-        public int TotalCopies { get; set; }
-        public int AvailableCopies { get; set; }
 
+        [Column("title")]
+        public string Title { get; set; }
+
+        [Column("author")]
+        public string Author { get; set; }
+
+        [Column("category")]
+        public string Category { get; set; }
+
+        [Column("total_copies")]
+        public int TotalCopies { get; set; }
+
+        [Column("available_copies")]
+        public int AvailableCopies { get; set; }
     }
 }
